9a88aad3a58a628f6f21c26a72da68b2ec5a4bc0
raquelsr <raquelserranor@hotmail.com>
2020-05-22 15:10:38 +0200

    Client-side: NavBar and UserList component

---
+++ client-side/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+      <v-toolbar-title>Home</v-toolbar-title>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'NavBar',
+
+  data: () => ({
+    //
+  }),
+};
+</script>
---
+++ client-side/src/components/UserList.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <h1>USER LIST</h1>
+      <p>{{msg}}</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'UserList',
+  props: ['msg'],
+  data: () => ({
+    //
+  }),
+};
+</script>
