747542c0663eca0b10ebbaa8d2986896301fc9a4
raquelsr <raquelserranor@hotmail.com>
2020-05-23 02:02:47 +0200

    Client-side: Models

---
+++ client-side/src/models/User.js
@@ -0,0 +1,11 @@
+class User {
+  constructor(firstName, lastName, email, birthDate, address) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+    this.birthDate = birthDate;
+    this.address = address;
+  }
+}
+
+export default User;
