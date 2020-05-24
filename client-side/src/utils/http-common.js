4e0214677633d77c68e35fb3da4bd518e3511ecc
raquelsr <raquelserranor@hotmail.com>
2020-05-24 20:56:24 +0200

    Change URL for deploy

--- client-side/src/utils/http-common.js
+++ client-side/src/utils/http-common.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:3001',
+  baseURL: 'https://user-appi-server.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },
